aeba9542e0a182d1c55962a186690c04aba9a045
Ruma Masuma <ruma.masuma@gmail.com>
2021-11-08 13:42:37 +0600

    remove local date string

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ try{
         // GET api using email
 app.get('/appointments' ,verifyToken, async(req, res)=>{
   const email = req.query.email;
-  const date = new Date(req.query.date).toLocaleDateString();
+  const date = req.query.date;
   // console.log(date);
   const query = {email: email , date : date};
   // console.log(query);
